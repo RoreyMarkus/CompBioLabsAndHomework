b3ecb4f41b629843f16cff7c72ced0cb6df641e8
RoreyMarkus <roma3653@colorado.edu>
2020-01-24 13:00:16 -0700

    Corrected Lab01

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,3 @@
 head PredPreyData.csv > PredPreyedited.csv
 tail PredPreyData.csv >> PredPreyedited.csv
-cut -f 2-4 -d , PredPreyData.csv  >> PredPreyedited.csv
+cut -f 2-4 -d , PredPreyedited.csv  >> PredPreyedited.csv
