27aea2aba90a594d38390553788b5c6da16eb94b
RoreyMarkus <roma3653@colorado.edu>
2020-01-28 19:58:12 -0700

    Corrected Lab01 again

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,4 @@
-head PredPreyData.csv > PredPreyedited.csv
-tail PredPreyData.csv >> PredPreyedited.csv
-cut -f 2-4 -d , PredPreyedited.csv  >> PredPreyedited.csv
+
+
+cut -f 2-4 -d , PredPreydata.csv  | grep time PredPreydata.csv > PredPreyedited.csv
+cut -f 2-4 -d , PredPreydata.csv  | tail PredPreydata.csv >> PredPreyedited.csv
